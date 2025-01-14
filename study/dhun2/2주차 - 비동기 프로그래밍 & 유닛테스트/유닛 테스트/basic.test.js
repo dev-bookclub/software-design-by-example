c8ec86f0abef441d35104a72e03c0dae9ee60380
Donghyun Park <dhp94d@gmail.com>
2025-01-14 21:11:33 +0900

    [dhyun2/2주차]: 학습 기록

---
+++ study/dhun2/2주차 - 비동기 프로그래밍 & 유닛테스트/유닛 테스트/basic.test.js
@@ -0,0 +1,45 @@
+import assert from 'assert';
+
+const HopeTests = [];
+let HopePass = 0;
+let HopeFail = 0;
+let HopeError = 0;
+
+const hopeThat = (message, callback) => {
+  HopeTests.push([message, callback]);
+};
+
+const main = () => {
+  HopeTests.forEach(([message, test]) => {
+    try {
+      test();
+      HopePass += 1;
+    } catch (e) {
+      if (e instanceof assert.AssertionError) {
+        HopeFail += 1;
+      } else {
+        HopeError += 1;
+      }
+    }
+  });
+  console.log(`pass ${HopePass}`);
+  console.log(`fail ${HopeFail}`);
+  console.log(`error ${HopeError}`);
+};
+
+const sign = (value) => {
+  if (value < 0) {
+    return -1;
+  } else {
+    return 1;
+  }
+};
+
+hopeThat(`Sign of negative is -1`, () => assert(sign(-3) === -1));
+hopeThat(`Sign of positive is 1`, () => assert(sign(19) === 1));
+
+hopeThat(`Sign of zero is 0`, () => assert(sign(0) === 0));
+
+hopeThat(`Sign misspelled is error`, () => assert(sgn(1) === 1));
+
+main();
